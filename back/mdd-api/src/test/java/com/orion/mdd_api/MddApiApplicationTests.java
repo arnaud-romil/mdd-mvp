53dbc3cf1b005abec5a8e21c2a6f4d781d4d2fab
Arnaud ROMIL <arnaud.romil@gmail.com>
2025-02-04 11:32:34 +0100

    init backend project

---
+++ back/mdd-api/src/test/java/com/orion/mdd_api/MddApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orion.mdd_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MddApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
