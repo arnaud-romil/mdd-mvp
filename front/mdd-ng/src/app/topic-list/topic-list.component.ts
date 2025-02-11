400f6db3e651d628867638c68b0f292c869d5f4d
Arnaud ROMIL <arnaud.romil@gmail.com>
2025-02-11 10:40:05 +0100

    front install material

--- front/mdd-ng/src/app/topic-list/topic-list.component.ts
+++ front/mdd-ng/src/app/topic-list/topic-list.component.ts
@@ -2,19 +2,26 @@ import { Component } from '@angular/core';
 import { TopicService } from '../services/topic.service';
 import { AsyncPipe } from '@angular/common';
 import { TopicComponent } from "../topic/topic.component";
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-topic-list',
   standalone: true,
-  imports: [AsyncPipe, TopicComponent],
+  imports: [
+    MatButtonModule
+    /*
+    AsyncPipe, 
+    TopicComponent
+    */
+  ],
   templateUrl: './topic-list.component.html',
   styleUrl: './topic-list.component.css'
 })
 export class TopicListComponent {
 
 
-  constructor(private readonly topicService: TopicService) { }
+  //constructor(private readonly topicService: TopicService) { }
 
-  topics$ = this.topicService.topics$;
+  //topics$ = this.topicService.topics$;
 
 }
