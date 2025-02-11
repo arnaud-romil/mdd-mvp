626f4a6a37f6a6170b79e6d85b8d9cf6fa793f25
Arnaud ROMIL <arnaud.romil@gmail.com>
2025-02-11 10:57:47 +0100

    add front home page

---
+++ front/mdd-ng/src/app/features/home/home.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [MatButtonModule, MatCardModule, RouterModule],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+}
